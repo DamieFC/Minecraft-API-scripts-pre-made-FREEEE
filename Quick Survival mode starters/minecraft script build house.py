e601c71bd59d1e5cf8e15e1149b68418542dd4a0
DamieFC <69485115+DamieFC@users.noreply.github.com>
2020-10-03 16:27:06 -0700

    Update minecraft script build house.py
    Changed some things and added things that made it more interactive....
    NOTE: If you want to do it in the same world, take out the mc.Minecraft.createWorld()
    ANOTHER NOTE: I think that mc.Minecraft.createWorld() is wrong, if it is, change it please. Thanks!

--- Quick Survival mode starters/minecraft script build house.py
+++ Quick Survival mode starters/minecraft script build house.py
@@ -1,3 +1,6 @@
 from mcpi import minecraft
+mc.Minecraft.createWorld() #I think that's how you do it?
+mc.postToChat("Creating cube...")
 mc.setBlocks(-4, -4, -4, 4, 4, 4, block.STONE.id)
+mc.postToChat("Creating air...")
 mc.setBlocks(-2, -2, -2, 2, 2, 2, block.AIR.id)
