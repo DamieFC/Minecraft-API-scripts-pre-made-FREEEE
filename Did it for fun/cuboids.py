2cd0b5f0912ffcae18053eb62d1cf8a52fd86372
DamieFC <69485115+DamieFC@users.noreply.github.com>
2020-10-21 08:09:55 -0700

    Create cuboids.py
    I did it for fun

---
+++ Did it for fun/cuboids.py
@@ -0,0 +1,6 @@
+# Creates house out of STONE, reminder, not hollow, you have to carve out the inside
+mc.setBlocks(-1, -1, -1, 1, 1, 1, block.STONE.id)
+# Creates house out of water in the same EXACT place
+mc.setBlocks(-1, -1, -1, 1, 1, 1, block.WATER.id)
+# Creates house out of TNT (Why, I don't know.You try to  carve out a front door, KABOOM!)
+mc.setBlocks(-1, -1, -1, 1, 1, 1, block.TNT.id)
